f7d009f8fc4ea730940612f66ad3d46e95c5a012
samaralkhateb <samar.alkhateb@gmail.com>
2020-03-04 02:36:31 +0200

    update

--- application/views/footer.php
+++ application/views/footer.php
@@ -1,6 +1,41 @@
-<div id="template" style="display: none"> </div>
+<!-- Footer -->
+<footer class="page-footer font-small unique-color-dark pt-4">
+
+  <div class="container">
 
 
-<script type="text/javascript" src="<?php echo base_url() ."assets/js/plugins/jquery/jquery.js"?>"></script>
-<script type="text/javascript" src="<?php echo base_url() ."assets/js/plugins/jquery/jquery-ui.min.js"?>"></script>
-<script type="text/javascript" src="<?php echo base_url() ."assets/js/plugins/bootstrap/bootstrap.min.js"?>"></script>
+                    <div class="footer-copyright text-center py-2">
+                        <div class="text-center" id="locationSpace">أنت تستخدم : <a id="locationLink" src="#changeLocation"> شمرا- سوريا</a></div>
+
+                    </div>
+
+                    <ul class="list-unstyled list-inline text-center py-2 footer">
+
+                         <a href="/terms">سياسة الخصوصية</a>
+
+
+                      <a href="/about">من نحن</a></li>
+
+
+
+                         <a href="/faq">مساعدة</a>
+
+
+                         <a href="/contactus/">اتصل بنا</a>
+
+                     <a href="https://www.facebook.com/shamrasy"><i class="fa fa-facebook-official facebook-">
+                                 Facebook</i></a>
+                   <a href="https://twitter.com/ShamraSyria"><i class="fa fa-twitter-square twitter-">
+                                 Twitter</i></a>
+                 </ul>
+
+  </div>
+  <div class="footer-copyright text-center py-3">© 2020</div>
+  <!-- Copyright -->
+
+</footer>
+<!-- Footer -->
+
+<script type="text/javascript" src="<?php echo base_url() . "/assets/js/plugins/bootstrap/bootstrap.min.js" ?>" /></script>
+<script type="text/javascript" src="<?php echo base_url() . "/assets/js/plugins/jquery/jquery.min.js" ?>" /></script>
+<script type="text/javascript" src="<?php echo base_url() . "/assets/js/plugins/bootstrap/bootstrap.min.js" ?>" /> </script>
--- application/views/header.php
+++ application/views/header.php
@@ -1,66 +1,56 @@
-<!DOCTYPE html>
+  <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <!-- META SECTION -->
-     <!-- META SECTION -->
-     <title> Google Istanbul</title>
-        <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-        <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-         <!-- CSS File -->
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.5.0/css/font-awesome.min.css" type="text/css" />
- 
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css" integrity="sha384-HSMxcRTRxnN+Bdg0JdbxYKrThecOKuH5zCYotlSAcp1+c8xmyTe9GYg1l9a69psu" crossorigin="anonymous">
+    <head>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <meta name="viewport" content="width=device-width, initial-scale=1">
+       <title>Google Istanbul</title>
+       <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
 
-         <!-- Optional theme -->
-         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap-theme.min.css" integrity="sha384-6pzBo3FDv/PJ8r2KRkGHifhEocL+1X2rVCTTkUfGk7/0pbek5mMa1upzvWbrUbOZ" crossorigin="anonymous">
-      <!-- datetimepicker -->
+      <link rel="stylesheet" href="<?php echo base_url() . "/assets/css/bootstrap/bootstrap.min.css" ?>" />
 
+ <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/style.css" ?>" />
 
+<nav class="my_nav navbar navbar-expand-sm bg-light navbar-light fixed-top">
 
-
-          <link rel="stylesheet" type="text/css" href="<?php echo base_url() . "assets/css/style.css" ?>" />
-
-      </head>
- <body>
-
-
-
-   <nav class="my_nav navbar navbar-expand-sm bg-light navbar-light">
-
-                     <a class="nav-link" href="/">
-                         <img class="img-responsive" src=<?php echo base_url() . "/assets/icons/png-white/shamra.svg" ?> height="35px"  alt="shamra-logo">
-                     </a>
+                  <a class="nav-link" href="/">
+                      <img class="img-responsive" src=<?php echo base_url() . "/assets/icons/png-white/shamra.svg" ?> height="35px"  alt="shamra-logo">
+                  </a>
 
 
 
 <ul class="navbar-nav font">
 
-  <li class="nav-item"><a class="nav-link" href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/BAZAAR.png" ?> alt="" width="16px"> بازار</a></li>
-                    <li class="nav-item"><a class="nav-link " href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/JOBS.png" ?> alt="" width="16px"> فرص عمل</a></li>
-  <li class="nav-item"><a class="nav-link" href=""> <i class="fa fa-users"></i> ناس</a></li>
-                      <li ><a class="nav-link" href="#"> <i class="fa fa-map-marker"></i> الأماكن</a></li>
-                    <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-newspaper-o" aria-hidden="true"></i> الأخبار</a></li>
-                    <li class="nav-item"><a class="nav-link " href="#"> <i class="fa fa-graduation-cap"></i> أكاديميا</a></li>
-                      <li class="nav-item"><a class="nav-link" href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/translate.png" ?> alt="" width="16px"> الترجمة</a></li>
+<li class="nav-item"><a class="nav-link" href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/BAZAAR.png" ?> alt="" width="16px"> بازار</a></li>
+                 <li class="nav-item"><a class="nav-link " href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/JOBS.png" ?> alt="" width="16px"> فرص عمل</a></li>
+<li class="nav-item"><a class="nav-link" href=""> <i class="fa fa-users"></i> ناس</a></li>
+                   <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-map-marker"></i> الأماكن</a></li>
+                 <li class="nav-item"><a class="nav-link" href="#"> <i class="fa fa-newspaper-o" aria-hidden="true"></i> الأخبار</a></li>
+                 <li class="nav-item"><a class="nav-link " href="#"> <i class="fa fa-graduation-cap"></i> أكاديميا</a></li>
+                   <li class="nav-item"><a class="nav-link" href="#"> <img src=<?php echo base_url() . "/assets/icons/png-white/translate.png" ?> alt="" width="16px"> الترجمة</a></li>
 
-                      <li class="nav-item dropdown">
-                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                          <i class="fa fa-bars"></i> المزيد <span class="caret"></span></a>
-                              <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                                 <a class="dropdown-item" href="#"> اختصار الروابط</a>
-                                <a class="dropdown-item" href="#">شمرا أعمال</a>
-                                <a class="dropdown-item" href="#">شمرا عملات</a>
-                                <a class="dropdown-item" href="#">شمرا ميديا</a>
-                                <a class="dropdown-item" href="#">إضافة موقع ويب</a>
-                                <a class="dropdown-item" href="#">شمرا مطورين</a>
-                              </div>
-                        </li>
+                   <li class="nav-item dropdown">
+                     <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                       <i class="fa fa-bars"></i> المزيد <span class="caret"></span></a>
+                           <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                             <a class="dropdown-item" href="#"> اختصار الروابط</a>
+                             <a class="dropdown-item" href="#">شمرا أعمال</a>
+                             <a class="dropdown-item" href="#">شمرا عملات</a>
+                             <a class="dropdown-item" href="#">شمرا ميديا</a>
+                             <a class="dropdown-item" href="#">إضافة موقع ويب</a>
+                             <a class="dropdown-item" href="#">شمرا مطورين</a>
+                           </div>
+                     </li>
+
+                     <li class="nav-item left"><a class="nav-link" href="#"> تسجيل الدخول </a></li>
+
 </ul>
 
 
 
 
-  </nav>
+</nav>
+    </head>
+    <body>
